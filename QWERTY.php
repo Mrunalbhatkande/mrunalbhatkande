0ac1a320b3b242d6b6f58ad7f49695da84ee453e
Mrunalbhatkande <89386398+Mrunalbhatkande@users.noreply.github.com>
2022-02-21 12:53:08 +0530

    Add files via upload

---
+++ QWERTY.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="name" id="name" placeholder="enter name">
+        <input type="text" name="age" id="age" placeholder="enter age">
+        <input type="text" name="gender" id="gender" placeholder="enter gender">
+        <input type="phone" name="phone" id="phone" placeholder="enter phone">
+        
+        <button class="btn">SUBMIT</button>
+
+</body>
+</html>
